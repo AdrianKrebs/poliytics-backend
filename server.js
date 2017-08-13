cdeaffabcac6994db6d386df24c24b3a21c8b099
Adrian Krebs <krebs-adrian@bluewin.ch>
2017-08-13 21:00:29 +0200

    fix

--- server.js
+++ server.js
@@ -22,45 +22,48 @@ const config = require('./config');
 const models = join(__dirname, 'app/models');
 const port = process.env.PORT || 3000;
 const uristring =
-    process.env.MONGODB_URI ||
+    process.env.MONGODB_URI || process.env.MONGOLAB_URI ||
     'mongodb://localhost/test';
 
+
+console.log('mongodb started on : ' + uristring);
+//  var options = { server: { socketOptions: { keepAlive: 1 } } };
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+        console.log('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log('Succeeded connected to: ' + uristring);
+    }
+});
+
 const app = express();
-const connection = connect();
 
 /**
  * Expose
  */
 
 module.exports = {
-  app,
-  connection
+    app
 };
 
 // Bootstrap models
 fs.readdirSync(models)
-  .filter(file => ~file.indexOf('.js'))
-  .forEach(file => require(join(models, file)));
+    .filter(file => ~file.indexOf('.js'))
+    .forEach(file => require(join(models, file)));
 
 // Bootstrap routes
 require('./config/passport')(passport);
 require('./config/express')(app, passport);
 require('./config/routes')(app, passport);
 
-connection
-  .on('error', console.log)
-  .on('disconnected', connect)
-  .once('open', listen);
 
-function listen () {
-  if (app.get('env') === 'test') return;
-  app.listen(port);
-  console.log('Express app started on port ' + port);
+function listen() {
+    if (app.get('env') === 'test') return;
+    app.listen(port);
+    console.log('Express app started on port ' + port);
 }
 
-function connect () {
-    console.log('mongodb started on : '+uristring);
-//  var options = { server: { socketOptions: { keepAlive: 1 } } };
-  var connection = mongoose.connect(uristring).connection;
-  return connection;
+function connect() {
+
+    return connection;
 }
