4a66e384d1b319555d4fcc1ab61382fad93e2082
Adrian Krebs <krebs-adrian@bluewin.ch>
2017-09-26 20:02:57 +0200

    delay

--- app/controllers/tweets.controller.js
+++ app/controllers/tweets.controller.js
@@ -44,7 +44,7 @@ setTimeout(function () {
         stream.on('error', streamError);
     });
 
-}, 20000); // time to restart to avoid connection limit reached exception
+}, 30000); // time to restart to avoid connection limit reached exception
 function streamFilter(data) {
     console.log('someone just tweeted!.....' + data.text);
 
@@ -258,7 +258,7 @@ setTimeout(function () {
         trackingStream.on('data', trackingFilter);
         trackingStream.on('error', trackingError);
     });
-}, 25000); // delay to avoid connection limit reached
+}, 60000); // delay to avoid connection limit reached
 
 function trackingFilter(tweet) {
     const politicianMentions = R.filter((mention) => twitterScreenNamesAsSet.has(mention.screen_name), tweet.entities.user_mentions);
